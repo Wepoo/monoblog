4a1a8f612c1a52d3296a3435bc4f5dda1b8a4503
Wepoo <yurkovetc@gmail.com>
2016-07-03 12:55:33 +0300

    Added posibilities to log in and sign up

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('monoblog', ['ui.router', 'templates'])
+angular.module('monoblog', ['ui.router', 'templates', 'Devise'])
 .config([
 '$stateProvider',
 '$urlRouterProvider',
@@ -24,6 +24,21 @@ function($stateProvider, $urlRouterProvider) {
           return posts.get($stateParams.id);
         }]
       }
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'views/_login.html',
+      controller: 'AuthCtrl'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'views/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('home');
+        })
+      }]
     });
   $urlRouterProvider.otherwise('home');
 }]);
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:create, :update, :destroy]
+  
   def index
     respond_with Post.all
   end
